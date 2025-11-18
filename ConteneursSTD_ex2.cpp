7bc68db887a59ce4e422cf44672cf7deb4d44952
TBAmphi <leandre.decq@gmail.com>
2025-11-18 21:36:00 +0100

    Conteneurs_ex 2

---
+++ ConteneursSTD_ex2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstdlib>
+#include <map>
+
+void FindInList(std::map<std::string, int> animaux, std::string animalName);
+void NbPattesFinder(std::map<std::string, int> animaux, int nbPattes);
+
+int main()
+{
+    std::map<std::string, int> animaux;
+    animaux["Chat"] = 4;
+    animaux["Humain"] = 2;
+    animaux["Araignee"] = 8;
+    animaux["Chien"] = 4;
+    animaux["Dodo"] = 2;
+    animaux["Mouton"] = 4;
+    animaux["Serpent"] = 0;
+
+    FindInList(animaux, "Chien");
+    FindInList(animaux, "Licorne");
+
+    FindInList(animaux, "Dodo");
+    animaux.erase("Dodo");
+    FindInList(animaux, "Dodo");
+
+    FindInList(animaux, "Mouton");
+    animaux["Mouton"] = 5;
+    FindInList(animaux, "Mouton");
+
+    NbPattesFinder(animaux, 4);
+}
+
+void FindInList(std::map<std::string, int> animaux, std::string animalName)
+{
+    std::map<std::string, int>::iterator found = animaux.find(animalName);
+    if (found != animaux.end()) 
+    {
+        std::cout << "Animal : " << (*found).first << std::endl;
+        std::cout << "Nombre de pattes : " << (*found).second << "\n" << std::endl;
+    }
+}
+
+void NbPattesFinder(std::map<std::string, int> animaux, int nbPattes)
+{
+    std::cout << "Voici les animaux a " << nbPattes << " pattes qui se trouvent dans la liste :" << std::endl;
+    std::map<std::string, int>::iterator it = animaux.begin();
+    while(it != animaux.end())
+    {
+        if((*it).second == nbPattes)
+        {
+            std::cout << (*it).first << std::endl;
+        }
+        it++;
+    }
+}
